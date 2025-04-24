5febb6054517955bc0f84d47e4e37afe8b4e1b2f
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-04-24 20:20:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const botTokens = [
 const statuses = ["dnd", "dnd", "dnd"];
 
 // Activity types: 0=Playing, 1=Streaming, 2=Listening, 3=Watching, 4=Custom, 5=Competing
-const activityTypes = [5, 4, 0]; // should be numbers, not strings
+const activityTypes = [0, 4, 0]; // should be numbers, not strings
 
 // Activity text
 const activityTexts = ["Roblox", "Sad", "Roblox"];
